39c77c3029f91224f5af4cf4459237d0d25c6f2b
keithlei01 <keithlei01@gmail.com>
2017-04-13 01:30:11 +0800

    Article page css

--- src/containers/ArticlePage.js
+++ src/containers/ArticlePage.js
@@ -1,5 +1,5 @@
-import React, {Component, PropTypes} from 'react'
-import {Link} from 'react-router'
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import configData from '../config.js';
 var Slider = require('react-slick');
@@ -51,7 +51,7 @@ export default class ArticlePage extends Component {
         urlArr = url.split("/");
         this.articleId = urlArr[urlArr.length - 1];
 
-        this.state = {divs: divs};
+        this.state = { divs: divs };
         this.htmlCategories = [];
         this.generateDivs = this.generateDivs.bind(this);
         this.count = 0;
@@ -74,7 +74,7 @@ export default class ArticlePage extends Component {
 
     categoriesClick(value) {
         var arr = [];
-        this.state = {divs: arr};
+        this.state = { divs: arr };
         this.count = 0;
         if (value == "All articles") {
 
@@ -87,21 +87,19 @@ export default class ArticlePage extends Component {
             $.get(configData.url + "api/article/category/" + value).done((res) => {
                 console.log(res);
 
-                this.state = {divs: divs};
+                this.state = { divs: divs };
                 arr = res.res;
                 this.res = arr;
 
                 this.generateDivs();
             });
         }
-
-
     }
 
     showBody(value) {
         if (value != null && value != "") {
             return (
-                <div dangerouslySetInnerHTML={{__html: value}}></div>
+                <div dangerouslySetInnerHTML={{ __html: value }}></div>
             )
         }
     }
@@ -112,37 +110,39 @@ export default class ArticlePage extends Component {
         $.get(configData.url + "api/article?articleId=" + this.articleId).done((res) => {
             res.res.post_date = moment(res.res.post_date).format("MMM D, YYYY");
             moreDivs.push(
-                <div className="general-box">
-                    <div className="col-md-12 col-sm-12" style={{"textAlign": "center","marginTop": "10px"}}>
-                        <div className="col-md-2 col-sm-2">
-                            <a>
-                                <img style={{"width": "82px","borderRadius":"9%"}} src="../lib/images/article/cat_2.jpg"/>
-                            </a>
-                        </div>
-                        <div className="col-md-10 col-sm-10 form-group" style={{"textAlign":"left"}}>
-                            <div className="col-md-12 col-sm-12">
-                                       <a> {res.res.category.name}</a>
+                <div className="general-box article-box-main">
+                    <ul className="article-detail-wrapper">
+                        <div className="col-md-12 col-sm-12" style={{ "textAlign": "center", "marginTop": "10px" }}>
+                            <div className="col-md-2 col-sm-2">
+                                <a>
+                                    <img style={{ "width": "82px", "borderRadius": "9%" }} src="../lib/images/article/cat_2.jpg" />
+                                </a>
                             </div>
-                            <div className="col-md-12 col-sm-12">
-                                       <label style={{"fontSize":"11px"}}>Posted on {res.res.post_date}</label>
+                            <div className="col-md-10 col-sm-10 form-group" style={{ "textAlign": "left" }}>
+                                <div className="col-md-12 col-sm-12">
+                                    <a> {res.res.category.name}</a>
+                                </div>
+                                <div className="col-md-12 col-sm-12">
+                                    <label style={{ "fontSize": "11px" }}>Posted on {res.res.post_date}</label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                     <div className="col-md-12 col-sm-12" style={{"textAlign": "left"}}>
+                        <div className="col-md-12 col-sm-12" style={{ "textAlign": "left" }}>
                             <h2 className="col-md-12 col-sm-12">
-                                        {res.res.title}
+                                {res.res.title}
                             </h2>
-                     </div>
-                    <div className="col-md-12 col-sm-12" style={{"textAlign": "center"}}>
-                        {this.showBody(res.res.preview)}
-                    </div>
+                        </div>
+                        <div className="col-md-12 col-sm-12" style={{ "textAlign": "center" }}>
+                            {this.showBody(res.res.preview)}
+                        </div>
+                    </ul>
                 </div>
             );
 
         });
 
         setTimeout(() => {
-            this.setState({divs: this.state.divs.concat(moreDivs)});
+            this.setState({ divs: this.state.divs.concat(moreDivs) });
         }, 500);
 
     }
@@ -163,28 +163,36 @@ export default class ArticlePage extends Component {
 
         return (
 
-            <div style={{"margin-top": "72px"}}>
-                <div className="col-md-1">
-                </div>
-                <div className="col-md-10">
-                    <div className="themeA-container">
-                        <div className="row">
-                            <div className="col-md-3" style={{"margin-top": "5px"}}>
-                                    
-                            </div>
-                            <div className="col-md-6" style={{"margin-top": "5px"}}>
-                                
-
-                               {this.state.divs}
-                            </div>
+            <div style={{ "margin-top": "80px" }}>
+                <div className="row">
+                    <div className="col-md-2 col-sm-2 col-xs-2" style={{ "width": "10%" }}></div>
+                    <div className="col-md-6 col-sm-6 col-xs-6" style={{ "width": "620px" }}>
+                        <div id="wall">
+                            {this.state.divs}
                         </div>
                     </div>
-                </div>
-                <div className="col-md-1">
-                </div>
+                    <div className="col-md-4 col-sm-4 col-xs-4" style={{ "width": "310px" }}>
+                        <div id="articles-slider" className="general-box" style={{ "height": "180px" }}>
 
+                        </div>
+                        <div id="articles-slider" className="general-box" style={{ "height": "180px" }}>
+
+                        </div>
+
+                        {/*
+                        <div>
+                            <div data-ng-include data-src="'/assets/app/views/utilities/hotArticlesUtility.html'"></div>
+                        </div>
+                        <div id="right-sidebar" ui-scrollfix="+{{rightSidebarTop}}">
+                            <div>
+                                <div data-ng-include data-src="'/assets/app/views/utilities/recommendedArticlesUtility.html'"></div>
+                            </div>
+                        </div>
+                        */}
+                    </div>
+
+                </div>
             </div>
-
         )
     }
 }
