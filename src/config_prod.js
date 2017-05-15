372badd59eba567bb42c10f9998dbe97b8aa56c2
keithlei01 <keithlei01@gmail.com>
2017-05-15 22:17:30 +0800

    deployment setup

--- src/config_prod.js
+++ src/config_prod.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
     SITE_URL: "http://www.minibean.com.hk/",
-    API_URL: "http://www.minibean.com.hk/"
+    API_URL: "http://ec2-54-169-166-235.ap-southeast-1.compute.amazonaws.com:9000/"
 });
